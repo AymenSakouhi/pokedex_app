0e301a57706af8cc7d93b667df54715307f12ada
A.Sakouhi <asakouhi@A.Sakouhi>
2023-07-27 01:59:38 +0200

    getting formatter to work on default

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,12 @@
-import * as React from 'react'
-import './App.css'
+import * as React from "react";
+import "./App.css";
 
 function App() {
-
-    return (
-      <>
-      <h1 className="text-3xl font-bold bg-white text-black">
-        Pokedex app
-    </h1>      
+  return (
+    <>
+      <h1 className="text-3xl font-bold bg-white text-black">Pokedex app</h1>
     </>
-    )
-  }
+  );
+}
 
-export default App
+export default App;
