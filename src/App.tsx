125a93604c85c91fa039b1d588252f4876618521
A.Sakouhi <asakouhi@A.Sakouhi>
2023-07-27 02:07:56 +0200

    testing the pokedex info page

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,18 @@ import * as React from "react";
 import "./App.css";
 
 function App() {
+  const [loading, setLoading] = React.useState<boolean>(true); // eslint-disable-line @typescript-eslint/no-unused-vars
+
+  const getPokeDexInfo = async (name: string) => {
+    const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${name}`);
+    const data = await response.json();
+    console.log(data);
+  };
+
+  React.useEffect(() => {
+    console.log("Hello world!");
+    getPokeDexInfo("pikachu");
+  }, []);
   return (
     <>
       <h1 className="text-3xl font-bold bg-white text-black">Pokedex app</h1>
